34ccec23beaab9033b96fb07e680cf0c2cab8679
Dolapothe.dev <90981208+Awwal234@users.noreply.github.com>
2022-06-12 19:06:21 +0100

    Add files via upload

---
+++ tail project/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			white: '#ffffff',
+			slate: '#F1F5F9',
+			indigo: '#4338CA',
+			slateit: '#64748B',
+			inner: '#CBD5E1',
+			log: '#3730A3',
+		},
+		extend: {},
+	},
+	plugins: [],
+};
